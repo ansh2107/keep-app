f4f650099429822aed7674f2645b9f7da1a79163
Ansul <ansulsharma1312@gmail.com>
2021-08-04 07:28:20 +0530

    corrected the path of home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from 'src/home/home/home.component';
+import { HomeComponent } from 'src/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
