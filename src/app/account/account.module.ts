12251d2c45faae80fe06fb7aaca456325838a51b
Ansul <ansulsharma1312@gmail.com>
2021-08-04 07:51:06 +0530

    Created layout, login and register component and added outes as children in app route module

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AccountComponent } from './account.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { AccountRoutingModule } from './account-routing.module';
+import { LayoutComponent } from './layout.component';
+import { LoginComponent } from './login.component';
+import { RegisterComponent } from './register.component';
 
 
 @NgModule({
-  declarations: [
-    AccountComponent
-  ],
   imports: [
-    CommonModule
+      CommonModule,
+      ReactiveFormsModule,
+      AccountRoutingModule
+  ],
+  declarations: [
+      LayoutComponent,
+      LoginComponent,
+      RegisterComponent
   ]
 })
 export class AccountModule { }
