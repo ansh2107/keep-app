a050968221d07977cb208ebed95d4973679196b7
Ansul <ansulsharma1312@gmail.com>
2021-08-04 09:18:45 +0530

    1. Implemented Register functionality for user. 2. Implemented fake backend interceptor to mock the backend and stored data in local storage 3. Imlplemented authservice for authentication purposes. 4. Implemented alert service to show error and success banners.

---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,33 @@
+﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+
+
+
+@Injectable({ providedIn: 'root' })
+export class AccountService {
+    private userSubject: BehaviorSubject<User>;
+    public user: Observable<User>;
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) {
+        this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user') as string));
+        this.user = this.userSubject.asObservable();
+    }
+
+    public get userValue(): User {
+        return this.userSubject.value;
+    }
+
+    register(user: User) {
+        return this.http.post(`${environment.apiUrl}/users/register`, user);
+    }
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from 'src/app/home/home.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './_components/alert.component';
+import { fakeBackendProvider } from './_helpers/fake-backend';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+     // provider used to create fake backend
+     fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
